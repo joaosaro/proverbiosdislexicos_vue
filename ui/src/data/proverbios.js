992512693eaba4ac5141a85ea601132431707109
João Saro <joaovlsaro@gmail.com>
2018-09-26 09:20:10 +0100

    Add fallback proverbios data

---
+++ ui/src/data/proverbios.js
@@ -0,0 +1,39 @@
+const Proverbios = [
+  {
+    'id': 1,
+    'part1': 'A cavalo dado',
+    'part2': 'não se olha o dente',
+    'language': 'pt-pt',
+    'active': 1
+  },
+  {
+    'id': 2,
+    'part1': 'Grão a grão',
+    'part2': 'enche a galinha o papo',
+    'language': 'pt-pt',
+    'active': 1
+  },
+  {
+    'id': 3,
+    'part1': 'Em Abril',
+    'part2': 'águas mil',
+    'language': 'pt-pt',
+    'active': 1
+  },
+  {
+    'id': 4,
+    'part1': 'Em casa de ferreiro',
+    'part2': 'espeto de pau',
+    'language': 'pt-pt',
+    'active': 1
+  },
+  {
+    'id': 5,
+    'part1': 'Mais vale um pássaro na mão',
+    'part2': 'que dois a voar',
+    'language': 'pt-pt',
+    'active': 1
+  }
+]
+
+export default Proverbios
