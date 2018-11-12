0cca981eba1b8f766d5d14cc4d9b570fd8ba364a
João Saro <joaovlsaro@gmail.com>
2018-11-12 23:48:31 +0000

    Break sentence method

--- ui/src/components/Proverbio.vue
+++ ui/src/components/Proverbio.vue
@@ -1,18 +1,18 @@
 <template>
   <main class="proverbio">
-    <div class="proverbio__text proverbio__text--one"
+    <p class="proverbio__text proverbio__text--one"
       :contenteditable="isProverbioEditable"
       @keyup="updateCustomProverbio(1)"
-      ref="proverbio1">
-      {{ proverbioDislexico.part1 }}
-    </div>
+      ref="proverbio1"
+      v-html="breakSentence(proverbioDislexico.part1)">
+    </p>
     <div class="proverbio__separator"></div>
-    <div class="proverbio__text"
+    <p class="proverbio__text proverbio__text--two"
       :contenteditable="isProverbioEditable"
       @keyup="updateCustomProverbio(2)"
-      ref="proverbio2">
-      {{ proverbioDislexico.part2 }}
-    </div>
+      ref="proverbio2"
+      v-html="breakSentence(proverbioDislexico.part2)">
+    </p>
   </main>
 </template>
 
@@ -37,6 +37,25 @@ export default {
     updateCustomProverbio (part) {
       if (!this.isProverbioEditable) return
       this.printProverbioPart([part, this.$refs.proverbio1.textContent])
+    },
+
+    breakSentence (sentence) {
+      if (sentence.length < 21) return sentence
+
+      var middle = Math.floor(sentence.length / 2)
+      var before = sentence.lastIndexOf(' ', middle)
+      var after = sentence.indexOf(' ', middle + 1)
+
+      if (before === -1 || (after !== -1 && middle - before >= after - middle)) {
+        middle = after
+      } else {
+        middle = before
+      }
+
+      var firstHalf = sentence.substr(0, middle)
+      var secondHalf = sentence.substr(middle + 1)
+
+      return `${firstHalf}<br>${secondHalf}`
     }
   }
 }
@@ -51,6 +70,7 @@ export default {
 
   &__text
     height: 40%
+    margin: 0
     font:
       size: 6vh
       weight: 700
@@ -64,6 +84,11 @@ export default {
     display: flex
     flex-direction: column
     justify-content: flex-end
+
+  &__text--two
+    display: flex
+    flex-direction: column
+    justify-content: flex-start
 
   &__separator
     height: 20%
